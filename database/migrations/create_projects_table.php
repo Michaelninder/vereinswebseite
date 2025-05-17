1f57a7112ec8535da058a64a968e4b0617e93fc9
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-17 13:52:29 +0200

    Create create_projects_table.php

---
+++ database/migrations/create_projects_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
